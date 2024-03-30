2a23d43b2b77b4e2a9a5c70bbb56d5f18507d2b7
compuvalpo@gmail.com <compuvalpo@gmail.com>
2024-03-30 20:28:29 -0300

    Add Boton Copiar
    Usando libreria Clipboard

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-
+	const clipboard = new ClipboardJS('.btnClip');
 });
 
 function SwalMensaje(title, text, icon, url) {
@@ -34,9 +34,14 @@ function GenerarPasswordHash(){
 						let div = '<div class="input-group col-12 mb-2"><span class="btn-primary badge d-flex align-items-center">';
 						
 						$('.card-data').html("");
-						$('.card-data').append(div +'COST 10 ['+ data.time10 +'s]</span><span class="form-control" id="cost10">'+ data.cost10 +'</span></div>');
-						$('.card-data').append(div +'COST 12 ['+ data.time12 +'s]</span><span class="form-control" id="cost12">'+ data.cost12 +'</span></div>');
-						$('.card-data').append(div +'COST 15 ['+ data.time15 +'s]</span><span class="form-control" id="cost15">'+ data.cost15 +'</span></div>');
+						$('.card-data').append(div +'COST 10 ['+ data.time10 +'s]</span><span class="form-control" id="cost10">'+ data.cost10 +'</span>'
+						+'<button class="input-group-text btnClip" id="btnCOST10" data-clipboard-text="'+ data.cost10 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						
+						$('.card-data').append(div +'COST 12 ['+ data.time12 +'s]</span><span class="form-control" id="cost12">'+ data.cost12 +'</span>'
+						+'<button class="input-group-text btnClip" id="btnCOST12" data-clipboard-text="'+ data.cost12 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						
+						$('.card-data').append(div +'COST 15 ['+ data.time15 +'s]</span><span class="form-control" id="cost15">'+ data.cost15 +'</span>'
+						+'<button class="input-group-text btnClip" id="btnCOST15" data-clipboard-text="'+ data.cost15 +'"><i class="fa-regular fa-copy"></i></button></div>');
 
 					}, 100);
 					//SwalMensaje('Password Hash Generados', 'Se han generado los Password Hash.', 'success');
